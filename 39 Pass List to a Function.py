e7d2ed1631a0867ea8de60d9f4a162d9be6ee7e2
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-12 21:53:03 +0530

    Each Python Topic with example for rookie programmer

--- 39 Pass List to a Function.py
+++ 39 Pass List to a Function.py
@@ -1,25 +1,19 @@
 # If user gives you list of elements you need to pass that  list in a function and that function will return value of number of odds and even number
 
 def odd_even_list(lst):
-
-    even =0
-    odd =0
+    even = 0
+    odd = 0
     for i in lst:
-        if i%2==0:
-            even +=1
+        if i % 2 == 0:
+            even += 1
         else:
-            odd +=1
+            odd += 1
 
-    return even,odd
-lst =[2,323,244,2,2323,3232]
-even,odd = odd_even_list(lst)
+    return even, odd
+
+
+lst = [2, 323, 244, 2, 2323, 3232]
+even, odd = odd_even_list(lst)
 
 print(even)
 print(odd)
-
-
-
-
-
-
-
