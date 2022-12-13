9b1ea3a2770afa4c2b001e20ce726a2365f4b878
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-13 21:44:33 +0530

    fibonacci series by taking input from the user till they need number less than they enter for eg if they enter 100, series will print from 0 ... 89

--- 40 Fibonacci Sequence.py
+++ 40 Fibonacci Sequence.py
@@ -1,24 +1,24 @@
 # Creating Fibonacci series function
 def fib(n):
-
     a = 0
     b = 1
 
-    if n<0:
+    if n < 0:
         print("Invalid number try positive number")
-        if n==1 :
+        if n == 1:
             print(a)
             print(b)
-    if n>1:
+    if n > 1:
         print(a)
         print(b)
 
-    for i in range(2,n+1):
-       c = a + b
-       a = b
-       b = c
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
 
-       if c<=n:
-           print(c)
+        if c <= n:
+            print(c)
+
 
 fib(100)
