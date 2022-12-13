e8473f738e4c4f02861a1a1c03e50de146f40c85
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-13 21:40:43 +0530

    Each Python Topic with example for rookie programmer

--- 40 Fibonacci Sequence.py
+++ 40 Fibonacci Sequence.py
@@ -1,7 +1,9 @@
 # Creating Fibonacci series function
 def fib(n):
+
     a = 0
     b = 1
+
     if n<0:
         print("Invalid number try positive number")
         if n==1 :
@@ -12,12 +14,11 @@ def fib(n):
         print(b)
 
     for i in range(2,n+1):
-
-
        c = a + b
        a = b
        b = c
+
        if c<=n:
            print(c)
 
-fib(-10)
+fib(100)
