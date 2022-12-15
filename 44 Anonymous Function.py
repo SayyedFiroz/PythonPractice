082c5d63c0a23d0561dc6f08666c2b2445443f1b
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-15 20:05:21 +0530

    anonymous function means that the function is without a name and has single expression

---
+++ 44 Anonymous Function.py
@@ -0,0 +1,17 @@
+# Python Lambda Functions are anonymous function means that the function is without a name.
+# lambda keyword is used to define an anonymous function in Python.
+
+# It has only one expression and f is object
+
+# addition using lamda function
+
+f = lambda a,b : a+b
+
+result = f(5,4)
+print(result)
+
+# squaring using lambda function
+
+s = lambda a :a*a
+result = s(5)
+print(result)
