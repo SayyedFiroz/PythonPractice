5266ee7ef4888ca38ebacf67d57af9595881c135
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-16 23:23:27 +0530

    We create user defined module to break the project into multi parts or reuse that module when we needed. we can import that functions.

---
+++ 47 UserDefinedModules.py
@@ -0,0 +1,20 @@
+# User Defined Module :You can create your own functions and classes, put them inside modules, You can now include hundreds of lines of code into any program just by writing a simple import statement.
+
+# We create user defined module to break the project into multi parts or reuse that module when we needed.
+
+# we are importing functions from calc fil
+
+
+from Calc import *
+
+
+a= 2
+b= 3
+
+c = add(a,b)
+d = div(a,b)
+
+print(d)
+print(c)
+
+
