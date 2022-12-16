c87b09751296b462969fcc5dbf5d0f13a508b0af
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-16 23:26:13 +0530

    It is part of #47 and #48 file. This file is created to perform some operation on them .

---
+++ Calc.py
@@ -0,0 +1,28 @@
+
+# 47 UDM
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+# 48 SVN
+
+def main():
+    print("Hello calculator")
+
+
+if __name__=="__main__":
+     main()
+
+
+
+
+
