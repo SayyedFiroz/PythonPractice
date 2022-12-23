513f78c67c1bded852fbf60a14e4c909f54fa093
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-23 22:06:49 +0530

    Difference bwtween inbuild class and user defined class

---
+++ 51 Class and Object.py
@@ -0,0 +1,17 @@
+# class : Blueprint for object
+
+# how to create class :Using class keyword and giving name  to class keyword
+
+# what does class contain  : it has attribute(varibles) and behaviour( methodsand functions)
+
+class Computer:
+    def config(self):
+        print("This is i3 11th gen ,1tb")
+
+a=8
+print(type(a))              # It's inbuild class
+
+
+com1 = Computer()            # Constructer
+
+print(type(com1))            # It's userdefind class
