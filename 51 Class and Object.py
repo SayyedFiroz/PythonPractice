2b2951bbdc58102961eb776e99abd6cd42885c59
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-23 22:30:13 +0530

    How to call object in opps.

--- 51 Class and Object.py
+++ 51 Class and Object.py
@@ -2,16 +2,32 @@
 
 # how to create class :Using class keyword and giving name  to class keyword
 
-# what does class contain  : it has attribute(varibles) and behaviour( methodsand functions)
+# what does class contain  : it had attributed(variables) and behaviour( methods and functions)
 
 class Computer:
     def config(self):
-        print("This is i3 11th gen ,1tb")
-
-a=8
-print(type(a))              # It's inbuild class
+        print("i3 ,1tb")
 
 
-com1 = Computer()            # Constructer
+com1 = Computer()                               # Constructor
+com2 = Computer()
 
-print(type(com1))            # It's userdefind class
+
+
+# how to call method from class
+# Syntax : Class name.method name(Object name)
+
+# Different object will have different behave that's why we need to put which object we want.
+
+
+# 1st way
+
+Computer.config(com1)
+Computer.config(com2)
+
+# 2nd way (it take as argument)
+
+com1.config()
+com2.config()
+
+
